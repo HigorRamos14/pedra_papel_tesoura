98e6a6a1568fd60a3a373c16c1811f666e38e1e4
Higor Ramos <higorramosg@gmail.com>
2022-05-07 15:28:49 -0300

    1° versao incompleta
    versao com primeira tela funcionando e incompleta

--- ppt2.py
+++ ppt2.py
@@ -1,50 +1,45 @@
 from tkinter import *
-import random
-scrplayer = 0
-def pedra():
-    if ran == 1:
-        scrplayer = scrplayer + 0
-    elif ran == 2:
-        scrplayer = scrplayer - 1
-    else:
-        scrplayer = scrplayer + 1
 
-def papel():
-    if ran == 1:
-        scrplayer = scrplayer + 1
-    elif ran == 2:
-        scrplayer = scrplayer + 0
-    else:
-        scrplayer = scrplayer - 1
+class pageAll():
+    def __init__(self):
+        self.page_All = Tk()
+        self.page_All.geometry('600x700')
+        self.page_All.config(background='gray')
+        self.page_All.resizable(0, 0)
 
-def tesoura():
-    if ran == 1:
-        scrplayer = scrplayer - 1
-    elif ran == 2:
-        scrplayer = scrplayer + 1
-    else:
-        scrplayer = scrplayer + 0
+        self.button_First_Page()
 
-def ppt():
-    jan.destroy()
-    jan2 = Tk()
-    jan2.geometry('720x620')
-    jan2.title('pedra papel tesoura')
+        self.page_All.mainloop()
+    
+    def button_First_Page(self):
 
-    botaopedra = Button(jan2, text='pedra', command=pedra)
-    while scrplayer < 5:
-        ran = random.randint(1, 3)
-        botaopedra.pack
+        self.play_First_Page_Button = Button(
+            self.page_All, 
+            text='P L A Y',
+            command=self.game_Page,
+            width=20,
+        )
+        self.play_First_Page_Button.place(x=220, y=600)
 
+    def game_Page(self):
 
+        self.play_First_Page_Button.destroy()
+
+        self.button_Game_Page()
+
+    def button_Game_Page(self):
+
+        self.rock_Button = Button(
+            self.page_All, 
+            text='P E D R A', 
+            command=quit
+        )
+        self.rock_Button.pack()
 
 
-    jan2.mainloop()
 
-jan = Tk()
-jan.geometry('300x400')
-jan.title('pedr papel e tesoura')
-botao = Button(jan, text='play', command=ppt)
-botao.place(x=130,
-            y=250)
-jan.mainloop()
+class pptFunctions():
+    pass
+
+
+pageAll()
